45eac27c4dfa0d3ec9bef3a82073a5fe49d52186
lalad <lalad@DNJ>
2022-08-23 14:49:23 +0100

    Add project files.

---
+++ DataAcccess/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data.SqlClient;
+using Microsoft.VisualBasic;
+
+namespace DataAcccess
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to my first database app");
+            ConnectDB();
+            Console.ReadKey();
+        }
+
+
+       static void ConnectDB()
+        {
+            string connString = "Data Source=.;Initial Catalog=AdventureWorksLT2019;Integrated Security=True";
+            
+            SqlConnection connect = new SqlConnection(connString);
+            connect.Open();
+            string sql = "select top 10 * from SalesLT.Product";
+            SqlCommand cmd = new SqlCommand(sql,connect);
+
+            SqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine($"the product is {reader["Name"]} and price is {reader["Listprice"]}. Thanks");
+            }
+
+
+
+        }
+    }
+
+    
+}
